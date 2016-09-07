075b68ca591c9c7751fd6c7cf74dfa0c3abb74d3
imran <imufun1@gmail.com>
2016-09-07 15:00:33 +0600

    The create() method 0)The render() method 1)The dispose() 2) Having fun with the debugger and Code Hot Swapping In this section, we are
    Summary In this chapter, you learned a lot about LibGDX and how all the projects of an application work together. We covered LibGDX's backends, modules, and starter classes. Additionally, we covered what the application life cycle and corresponding interface are and how they are meant to work. The debugger has been used to inspect the demo application at runtime, and furthermore we made use of the  JVM Code Hot Swapping feature. We now know the basics of the LibGDX applications, so we are ready to start developing a real game. We will start at the very beginning of the development  cycle step by step. As LibGDX is a framework and not a game engine, we first  have to build our own engine. So, we will learn how to create an appropriate program architecture in the next chapter that is suitable to handle our gam

--- core/src/com/mygdx/canyonbunnymain/CanyonBunnyMain.java
+++ core/src/com/mygdx/canyonbunnymain/CanyonBunnyMain.java
@@ -2,11 +2,11 @@ package com.mygdx.canyonbunnymain;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class CanyonBunnyMain extends ApplicationAdapter {
     SpriteBatch batch;
@@ -19,14 +19,59 @@ public class CanyonBunnyMain extends ApplicationAdapter {
     public void create() {
         batch = new SpriteBatch();
         img = new Texture("badlogic.jpg");
+
+
+        float w = Gdx.graphics.getWidth();
+        float h = Gdx.graphics.getHeight();
+
+        camera = new OrthographicCamera(1, h / w);
+        img.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+        TextureRegion region = new TextureRegion(img, 0, 0, 512, 275);
+        sprite = new Sprite(region);
+        sprite.setSize(0.9f, 0.9f * sprite.getHeight() / sprite.getWidth());//Sets the size of the sprite when drawn, before scaling and rotation are applied.
+        sprite.setOrigin(sprite.getWidth() / 2, sprite.getHeight() / 2);//Sets the origin in relation to the sprite's position for scaling and rotation.
+        sprite.setPosition(-sprite.getWidth() / 2, -sprite.getHeight() / 2);//Sets the position where the sprite will be drawn.
+
+
     }
 
     @Override
+    public void dispose() {
+        // dispose()----> Releases all resources of this object.
+        batch.dispose();
+        img.dispose();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+
+    private float rot;
+
+    @Override
     public void render() {
-        Gdx.gl.glClearColor(1, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        // Gdx.gl.glClearColor(1, 0, 0, 1);
+        // Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.setProjectionMatrix(camera.combined);
         batch.begin();
+
+
+        final float degreesPerSecond = 100.0f;
+
+        rot = (rot + Gdx.graphics.getDeltaTime() * degreesPerSecond) % 90;
+
+        sprite.setRotation(rot);
+        sprite.draw(batch);
         batch.draw(img, 0, 0);
         batch.end();
     }
+
+
 }
