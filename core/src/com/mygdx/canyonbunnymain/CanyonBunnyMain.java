fde8f9f19604682febc5952635f23e23bddc0df4
imran <imufun1@gmail.com>
2016-09-07 16:58:06 +0600

    Implementing the Constants class  -Here is the listing of the code for Constants:
    Implementing the CanyonBunnyMain class - The following listing shows the first implementation of CanyonBunnyMain:
    
    Implementing the WorldController class -The following listing shows the first implementation of WorldController:

--- core/src/com/mygdx/canyonbunnymain/CanyonBunnyMain.java
+++ core/src/com/mygdx/canyonbunnymain/CanyonBunnyMain.java
@@ -1,45 +1,22 @@
 package com.mygdx.canyonbunnymain;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class CanyonBunnyMain extends ApplicationAdapter {
-    SpriteBatch batch;
-    Texture img;
 
-    private OrthographicCamera camera;
-    private Sprite sprite;
+
+    private static final String TAG = CanyonBunnyMain.class.getName();
+    private WorldController worldController;
+    private WorldRenderer worldRenderer;
+
 
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        img = new Texture("badlogic.jpg");
-
-
-        float w = Gdx.graphics.getWidth();
-        float h = Gdx.graphics.getHeight();
-
-        camera = new OrthographicCamera(1, h / w);
-        img.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
-        TextureRegion region = new TextureRegion(img, 0, 0, 512, 275);
-        sprite = new Sprite(region);
-        sprite.setSize(0.9f, 0.9f * sprite.getHeight() / sprite.getWidth());//Sets the size of the sprite when drawn, before scaling and rotation are applied.
-        sprite.setOrigin(sprite.getWidth() / 2, sprite.getHeight() / 2);//Sets the origin in relation to the sprite's position for scaling and rotation.
-        sprite.setPosition(-sprite.getWidth() / 2, -sprite.getHeight() / 2);//Sets the position where the sprite will be drawn.
-
-
     }
 
     @Override
     public void dispose() {
-        // dispose()----> Releases all resources of this object.
-        batch.dispose();
-        img.dispose();
+
     }
 
     @Override
@@ -52,25 +29,13 @@ public class CanyonBunnyMain extends ApplicationAdapter {
 
     }
 
+    @Override
+    public void pause() {
 
-    private float rot;
+    }
 
     @Override
     public void render() {
-        // Gdx.gl.glClearColor(1, 0, 0, 1);
-        // Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        batch.setProjectionMatrix(camera.combined);
-        batch.begin();
-
-
-        final float degreesPerSecond = 100.0f;
-
-        rot = (rot + Gdx.graphics.getDeltaTime() * degreesPerSecond) % 90;
-
-        sprite.setRotation(rot);
-        sprite.draw(batch);
-        batch.draw(img, 0, 0);
-        batch.end();
     }
 
 
