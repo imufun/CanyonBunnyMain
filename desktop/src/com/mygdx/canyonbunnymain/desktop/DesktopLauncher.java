37490f7b1ee0406baccec844c48d5900fb83bc8a
imran <imufun1@gmail.com>
2016-09-07 12:50:43 +0600

    game

---
+++ desktop/src/com/mygdx/canyonbunnymain/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.canyonbunnymain.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.canyonbunnymain.CanyonBunnyMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CanyonBunnyMain(), config);
+	}
+}
